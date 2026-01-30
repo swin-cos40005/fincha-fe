edebce350ff9b430031ea0ba0ae53574b2ce4a3c
Ky Anh Nguyen <anh163.nka@gmail.com>
2026-01-30 23:22:05 +0700

    feat(GRP8-197): Add github action for staging deployment

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -2,7 +2,7 @@ export function EmptyScreen() {
   return (
     <div className="flex items-center justify-center">
       <h1 className="text-4xl md:text-5xl font-bold text-center">
-        Hi, I'm Fincha.
+        Hi, I&apos;m Fincha.
       </h1>
     </div>
   )
