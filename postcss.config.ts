8c6e7e25ea3335122706eea733bc2f6a4fc2bf7f
Ky Anh Nguyen <anh163.nka@gmail.com>
2026-02-03 22:01:03 +0700

    chore: upgrade to next16

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'postcss-load-config'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 const config: Config = {
-  plugins: [tailwindcss, autoprefixer]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 }
 
 export default config
